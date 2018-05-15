4dfdff4c5af57bb3aa9159e41db60598a421d5d9
fridafu <31306289+fridafu@users.noreply.github.com>
2018-05-15 11:09:22 +0200

    better names and no h

--- src/RBM/main.cpp
+++ src/RBM/main.cpp
@@ -10,29 +10,29 @@ using namespace arma;
 int main(){
     vec dtvec = logspace<vec>(-4,0,51);
     double rho = 0.1;
-    double h = 0.0001;
-    int numpart = 2; //CHANGE THE NAME!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    int mc = (1048576 + 1000) / numpart; // monte carlo cycles
-    int howmanyDs = 2;
+    int numpart = 1; //CHANGE THE NAME!!!!!!!!!!!!!!!!!!!!!!!!!
+    int mc = 100000;//(1048576 + 1000) / numpart; // monte carlo cycles
+    int howmanyDs = 1;
     double hbar = 1;
     double mass = 1;
     double omega = 1;
     double sig = 1;
     double numM = howmanyDs*numpart;
-    double hidden = 2;
+    int hidden = 2;
     double dt = 0.001;
+    vec gammavec = {0.5, 1, 2, 3, 4};
+    double gamma;
 
     ofstream myfile;
     //myfile.open("interaction_N10.dat");
     //for(int elem=0; elem<size(dtvec,0); elem++){
     //    if(alphavec(elem) != 0.5){
     //dtvec(elem);
-    Solver S(hbar, mass, omega, rho, mc, numpart, howmanyDs, h, dt, sig, hidden, numM); // initialize Solver class
-    Bruteforce* B = new Bruteforce(hbar, mass, omega, rho, mc, numpart, howmanyDs, h, dt, sig, hidden, numM);
-    Impsamp* Imp = new Impsamp(hbar, mass, omega, rho, mc, numpart, howmanyDs, h, dt, sig, hidden, numM);
-    //Interact* Int = new Interact(hbar, mass, omega, rho, mc, numpart, howmanyDs, h, dt, sig, hidden, numM);
-    Gibbs* G = new Gibbs(hbar, mass, omega, rho, mc, numpart, howmanyDs, h, dt, sig, hidden, numM);
+    Solver S(hbar, mass, omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, numM); // initialize Solver class
+    Bruteforce* B = new Bruteforce(hbar, mass, omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, numM);
+    Impsamp* Imp = new Impsamp(hbar, mass, omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, numM);
+    //Interact* Int = new Interact(hbar, mass, omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, numM);
+    Gibbs* G = new Gibbs(hbar, mass, omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, numM);
     ofstream myfile2;
     ofstream myfile3;
     ofstream myfile4;
@@ -42,8 +42,15 @@ int main(){
     myfile2.open(filename + "_energy.dat");
 
     //B->solve();
-    B->best_params(myfile,myfile2);
-    B->go_brute(myfile, myfile2);
+    vec b = S.init_b();
+    mat w = S.init_w();
+    vec X = S.init_X();
+    vec a = S.init_a();
+    for(int i=0;i<5;i++){
+        gamma = gammavec(i);
+        B->best_params(myfile,myfile2,gamma,a,b,w,X);
+    }
+    //B->go_brute(myfile, myfile2);
 
     //B->solve(myfile, myfile2);
     //Imp->langevin(myfile,myfile2);
