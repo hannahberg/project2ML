e03028443627279e3e47bc26e41ed0a937fe205f
Frida Furmyr <31306289+fridafu@users.noreply.github.com>
2018-06-01 23:52:58 +0200

    Add files via upload

---
+++ results/data/brute_rho/plotenergy.py
@@ -0,0 +1,35 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+files_ = ['brute_N1_d1gam0.100000_H2_rho0.100000.dat', 'brute_N1_d1gam0.100000_H2_rho0.200000.dat', 'brute_N1_d1gam0.100000_H2_rho0.400000.dat', 'brute_N1_d1gam0.100000_H2_rho0.700000.dat', 'brute_N1_d1gam0.100000_H2_rho0.800000.dat', 'brute_N1_d1gam0.100000_H2_rho1.000000.dat']
+
+dtype1 = [('E', 'f8'), ('V','f8'), ('T', 'f8'), ('A', 'f8')]
+data_1 = loadtxt(files_[0],dtype=dtype1)
+data_2 = loadtxt(files_[1], dtype=dtype1)
+data_3 = loadtxt(files_[2], dtype=dtype1)
+data_4 = loadtxt(files_[3], dtype=dtype1)
+data_5 = loadtxt(files_[4], dtype=dtype1)
+data_6 = loadtxt(files_[5], dtype=dtype1)
+
+E_1 = data_1['E']
+E_2 = data_2['E']
+E_3 = data_3['E']
+E_4 = data_4['E']
+E_5 = data_5['E']
+E_6 = data_6['E']
+
+
+length = len(E_1)
+gdc = linspace(0,length-1,length)
+
+plt.plot(gdc,E_1)
+plt.hold('on')
+plt.plot(gdc,E_2)
+plt.plot(gdc,E_3)
+plt.plot(gdc,E_4)
+plt.plot(gdc,E_5)
+#plt.plot(gdc,E_6)
+plt.xlabel('GDC')
+plt.ylabel('Energy')
+plt.legend(['rho = 0.1','rho = 0.2','rho = 0.4','rho = 0.7','rho = 0.8'])
+plt.show()
