b54fc23762f04e34fe841de98d727c8817272acc
fridafu <31306289+fridafu@users.noreply.github.com>
2018-06-15 00:20:30 +0200

    trying to merge last version

--- src/RBM/main.cpp
+++ src/RBM/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include "solver.h"
 #include "bruteforce.h"
 #include "impsamp.h"
@@ -7,82 +7,102 @@
 using namespace std;
 using namespace arma;
 
+
 int main(){
-    double rho = 0.4;
-    int numpart = 1;
-    int mc = (1048576 + 1000) / numpart; // Monte Carlo Cycles
-    int howmanyDs = 1;
+    double rho = 1.0;
+    int numpart = 2;
+    int howmanyDs = 2;
+    int mc = (1048576 + 1000) / (numpart*howmanyDs); // Monte Carlo Cycles
     double omega = 1;
     double sig = 1;
     int hidden = 2;
-    double dt = 0.01;
+    double dt = 0.1;
     double gamma = 0.1;
-    int gdc = 1000; // Gradient Decent Cycles
-    bool interactionswitch = false;
-    double spread = 0.5;
+    int gdc = 5000; // Gradient Decent Cycles
+    bool interactionswitch = true;
+    double spread = 0.2;
 
-    vec gammavec = {0.01, 0.05, 0.1, 0.5, 1.0, 2.0};
-    //vec rhovec = {0.1,0.2,0.4,0.7,0.8,1.0};
-    //vec dtvec = logspace<vec>(-4,0,51);
+    //vec gammavec = {0.01, 0.05, 0.1, 0.5, 1.};
+    //vec rhovec = {0.1,0.3,0.6,0.8,1.0};
+    //vec dtvec = {0.001,0.005,0.01,0.1,1};
+    //vec hidvec = {0,1,2,3,5,10};
+    //vec numvec = {1,2};
+    //vec sigmavec = {0.6,0.7,0.8};
 
 
-    for(int i=0;i<6;i++){
+
+    for(int i=0;i<1;i++){
         //rho = rhovec(i);
-        gamma = gammavec(i);
+        //gamma = gammavec(i);
+        //hidden = hidvec(i);
+        //sig = sigmavec(i);
+        //numpart = numvec(i);
+
         Solver S(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread); // initialize Solver class
         string filename ="N" + std::to_string(numpart)+ "_d" + std::to_string(howmanyDs)+"gam" + std::to_string(gamma) + "_H" + std::to_string(hidden);
-
-        vec b = S.init_b(spread);
-        mat w = S.init_w(spread);
         vec X = S.init_X(); //GAUSSIAN IN IMPSAMP?
         vec a = S.init_a(spread);
+        vec b = S.init_b(spread);
+        mat w = S.init_w(spread);
 
+        gamma = 0.01;
+
+        filename ="N" + std::to_string(numpart)+ "_d" + std::to_string(howmanyDs)+"gam" + std::to_string(gamma) + "_H" + std::to_string(hidden);
         ofstream brutefile;
         ofstream brutefile2;
         brutefile.open("brute_" + filename + "_rho"+std::to_string(rho) +  ".dat");
-        //brutefile2.open("brute_" + filename + "_energy.dat");
         Bruteforce* B = new Bruteforce(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
         B->best_params(brutefile,brutefile2,gamma,a,b,w,X,gdc);
-        //brutefile2.close();
         brutefile.close();
 
+        gamma = 0.01;
+        filename ="N" + std::to_string(numpart)+ "_d" + std::to_string(howmanyDs)+"gam" + std::to_string(gamma) + "_H" + std::to_string(hidden);
         ofstream impfile;
         ofstream impfile2;
         impfile.open("imp_" + filename + "_dt"+std::to_string(dt) + ".dat");
-        //impfile2.open("imp_" + filename + "_dt"+std::to_string(dt) + "_energy.dat");
         Impsamp* I = new Impsamp(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
         I->best_params(impfile,impfile2,gamma,a,b,w,X,gdc);
-        //impfile2.close();
         impfile.close();
 
-//        ofstream gibfile;
-//        ofstream gibfile2;
-//        gibfile.open("gibbs_" + filename + "_sig"+std::to_string(sig) + ".dat");
-        //gibfile2.open("gibbs_" + filename + "_energy.dat");
-//        Gibbs* G = new Gibbs(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
-//        G->best_params(gibfile,gibfile2,gamma,a,b,w,gdc);
-        //gibfile2.close();
-//        gibfile.close();
+        gamma = 0.01;
+        sig = 0.7;
+        filename ="N" + std::to_string(numpart)+ "_d" + std::to_string(howmanyDs)+"gam" + std::to_string(gamma) + "_H" + std::to_string(hidden);
+        Solver S2(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
+
+        //b = S2.init_b(spread);
+        //w = S2.init_w(spread);
+        ofstream gibfile;
+        ofstream gibfile2;
+        gibfile.open("gibbs_" + filename + "_sig"+std::to_string(sig) + ".dat");
+        Gibbs* G = new Gibbs(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
+        G->best_params(gibfile,gibfile2,gamma,a,b,w,gdc);
+        gibfile.close();
 
 
-//       B->solve(a, b, w, X, myfile, myfile2);
-
-        //delete G;
+        delete G;
         delete I;
         delete B;
-        // Illuminati!
+/*
 
-        /*
-        vec b = {0.000667904,0.00171753};
-        mat w = {{0.00117447, 0.000381313},{-0.00166561, -0.000559385},{-0.000244628, -0.00130079}, {0.00103996, -0.00180347}};
-        vec X = {1,2,3,4};
-        vec X = {-0.436988, -0.243587,0.22773,0.146376};
-        vec a ={0.000586069,0.000838821,0.000520396,0.000574091};
-        */
+    }
+    hidden = 1;
+    for(int k=0;k<5;k++){
+        gamma = gammavec(k);
+
+        Solver S3(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread); // initialize Solver class
+        string filename ="N" + std::to_string(numpart)+ "_d" + std::to_string(howmanyDs)+"gam" + std::to_string(gamma) + "_H" + std::to_string(hidden);
+        ofstream impfile3;
+        ofstream impfile4;
+        impfile3.open("imp_" + filename + "_dt"+std::to_string(dt) + ".dat");
+        Impsamp* I = new Impsamp(omega, rho, mc, numpart, howmanyDs, dt, sig, hidden, interactionswitch, spread);
+        I->best_params(impfile3,impfile4,gamma,a,b,w,X,gdc);
+        impfile3.close();
+
+        delete I;
     }
 
 
-
+*/
 //    double exact_vint = 1./sqrt(2) + 1./sqrt(18)+1./sqrt(8);
 //    vec Xtry = {0, 0, 1, 1, 3, 3, 7,7};
 //    cout << scientific << exact_vint << endl;
@@ -96,4 +116,5 @@ int main(){
     // vary learning rate
     // vary hidden nodes, in non-interaction case H = 0 is best
     // interaction - 5 nodes?
+}
 }
